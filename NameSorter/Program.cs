eccfa29eee82af5dcc9e80b714fddf40579f52a5
Matthew Books <matthew_books@hotmail.com>
2025-09-24 20:43:51 +1000

    feat: Wire up file reader into main, add default unsorted names

--- NameSorter/Program.cs
+++ NameSorter/Program.cs
@@ -1,3 +1,25 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using NameSorter;
+
+IFileReader fileReader;
+
+var argsCount = args.Length;
+if (argsCount == 1)
+{
+    //TODO: Move instantiation into IoC container
+    fileReader = new FileReader();
+    var fileContents = fileReader.ReadAllLines(args[0]);
+
+    if (fileContents == null)
+    {
+        Console.WriteLine("Invalid file path provided");
+        Environment.Exit(1);
+    }
+    
+    Array.ForEach(fileContents, Console.WriteLine);
+}
+else
+{
+    Console.WriteLine($"Incorrect number of command line arguments ({argsCount}) provided");
+}
