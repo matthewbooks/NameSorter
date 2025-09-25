b162182524d3ce98c8dd5a12b939271518ba789f
Matthew Books <matthew_books@hotmail.com>
2025-09-25 20:41:37 +1000

    feat: Add Person object

---
+++ NameSorter/Person.cs
@@ -0,0 +1,7 @@
+namespace NameSorter;
+
+public class Person(string givenNames, string lastName)
+{
+    public string GivenNames { get; set; } = givenNames;
+    public string LastName { get; set; } = lastName;
+}
